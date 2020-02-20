a994cbfa4a4e8eebeeffdc0e06d08c9ddcf9da9d
Aja Blanco <59661417+ajablanco@users.noreply.github.com>
2020-02-20 14:57:26 -0800

    updated everything and reached stretch

---
+++ src/components/NasaCardTitle.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {CardBody, CardTitle} from 'reactstrap';
+
+const NasaCardTitle = (props) => {
+    return (
+        <CardBody>
+            <CardTitle>
+                
+                <h3>{props.imageTitle}</h3>
+               
+
+            </CardTitle>
+        </CardBody>
+    )
+}
+
+export default NasaCardTitle; 
